6cb63a835cf9fb612f59bea1539dc58d6e116207
WillZ <williamzhang515@gmail.com>
2026-06-21 15:58:39 +0800

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -39,20 +39,10 @@ module.exports = {
 
   // [OPTIONAL] This is where you specify the folders that are password protected. It is an array of paths pointing to all
   // the directories in which you have .password set. Check the documentation for details.
-  protectedRoutes: ['/Private', '/Demo/😎Another Private Folder Password 123'],
+  protectedRoutes: ['/Private'],
 
   // [OPTIONAL] Use "" here if you want to remove this email address from the nav bar.
   email: '',
-
-  // [OPTIONAL] This is an array of names and links for setting your social information and links.
-  // In the latest update, all brand icons inside font awesome is supported and the icon to render is based on the name
-  // you provide. See the documentation for details.
-  links: [
-    {
-      name: 'GitHub',
-      link: 'https://github.com/lyc8503',
-    },
-  ],
 
   // This is a day.js-style datetime format string to format datetimes in the app. Ref to
   // https://day.js.org/docs/en/display/format for detailed specification. The default value is ISO 8601 full datetime
